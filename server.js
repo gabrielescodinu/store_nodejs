808fa748f434fd0808b595274ef72a295b273e0a
Gabriele S <web@pezzilli.com>
2023-03-02 12:33:45 +0100

    push

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const mysql = require('mysql');
 const express = require('express');
-const studentController = require('./studentController');
-const userController = require('./userController');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const app = express();
+
+const studentController = require('./studentController');
+const userController = require('./userController');
 const db = require('./config');
+
 const port = 3000;
 
 // configuration middleware and express application ---------------------------------------------------------------------------------------------------------------------------------
@@ -20,74 +22,28 @@ app.use(session({
     saveUninitialized: true
 }));
 
-// login ---------------------------------------------------------------------------------------------------------------------------------
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    // Check if the username exists in the database
-    const sql = 'SELECT * FROM users WHERE username = ?';
-    db.query(sql, [username], (err, result) => {
-        if (err) throw err;
-
-        if (result.length === 1) {
-            const user = result[0];
-
-            // Check if the password is correct
-            if (user.password === password) {
-                req.session.loggedIn = true;
-                res.redirect('/dashboard');
-            } else {
-                res.send('Invalid username or password');
-            }
-        } else {
-            res.send('Invalid username or password');
-        }
-    });
-});
-
-// logout ---------------------------------------------------------------------------------------------------------------------------------
-app.get('/logout', (req, res) => {
-    // Destroy the user's session
-    req.session.destroy(err => {
-        if (err) throw err;
-
-        // Redirect the user to the login page
-        res.redirect('/login');
-    });
-});
-
-// registration ---------------------------------------------------------------------------------------------------------------------------------
-app.post('/registration', (req, res) => {
-    userController.createUser(req, res, db);
-});
-
-app.get('/registration', userController.getCreateUserPage);
-
 // homepage ---------------------------------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// dashboard ---------------------------------------------------------------------------------------------------------------------------------
-const requireLogin = (req, res, next) => {
-    if (req.session.loggedIn) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-};
-
-app.get('/dashboard', requireLogin, (req, res) => {
-    res.render('dashboard');
+// user ---------------------------------------------------------------------------------------------------------------------------------
+// login
+app.get('/login', userController.login);
+app.post('/login', userController.loginUser);
+// logout
+app.get('/logout', userController.logout);
+// registration
+app.post('/registration', (req, res) => {
+    userController.createUser(req, res, db);
 });
+app.get('/registration', userController.getCreateUserPage);
+// dashboard
+app.get('/dashboard', userController.requireLogin, userController.dashboard);
 
 // students ---------------------------------------------------------------------------------------------------------------------------------
 app.post('/student-create', (req, res) => studentController.storeStudent(req, res, db));
-app.get('/student-create', requireLogin, studentController.createStudent);
+app.get('/student-create', userController.requireLogin, studentController.createStudent);
 app.get('/students', (req, res) => studentController.getStudents(req, res, db));
 app.post('/student-edit', (req, res) => studentController.editStudent(req, res, db));
 app.post('/students/:id/update', (req, res) => studentController.updateStudent(req, res, db));
--- userController.js
+++ userController.js
@@ -1,4 +1,4 @@
-// userController.js
+// registration ---------------------------------------------------------------------------------------------------------------------------------
 
 function createUser(req, res, db) {
     const { username, password } = req.body;
@@ -16,4 +16,60 @@ function getCreateUserPage(req, res) {
     res.render('registration');
 }
 
-module.exports = { createUser, getCreateUserPage };
+// login ---------------------------------------------------------------------------------------------------------------------------------
+const db = require('./config');
+
+const login = (req, res) => {
+    res.render('login');
+};
+
+const loginUser = (req, res) => {
+    const { username, password } = req.body;
+
+    // Check if the username exists in the database
+    const sql = 'SELECT * FROM users WHERE username = ?';
+    db.query(sql, [username], (err, result) => {
+        if (err) throw err;
+
+        if (result.length === 1) {
+            const user = result[0];
+
+            // Check if the password is correct
+            if (user.password === password) {
+                req.session.loggedIn = true;
+                res.redirect('/dashboard');
+            } else {
+                res.send('Invalid username or password');
+            }
+        } else {
+            res.send('Invalid username or password');
+        }
+    });
+};
+
+// logout ---------------------------------------------------------------------------------------------------------------------------------
+const logout = (req, res) => {
+    // Destroy the user's session
+    req.session.destroy(err => {
+        if (err) throw err;
+
+        // Redirect the user to the login page
+        res.redirect('/login');
+    });
+};
+
+
+
+const requireLogin = (req, res, next) => {
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+const dashboard = (req, res) => {
+    res.render('dashboard');
+};
+
+module.exports = { createUser, getCreateUserPage, login, loginUser, logout, requireLogin, dashboard };
